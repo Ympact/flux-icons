f254512b32036a5c50b2cf78ef1c395a5a3ebe80
Maurits <maurits@ympact-technologies.nl>
2024-10-16 09:58:48 +0200

    fix: correct config file

--- config/flux-icons.php
+++ config/flux-icons.php
@@ -1,51 +1,82 @@
 <?php
 
-use Ympact\FluxIcons\Helpers\FluxIconHelper;
+use Ympact\FluxIcons\DataTypes\Icon;
+use Ympact\FluxIcons\DataTypes\SvgPath;
 
 return [
-    'tabler' => [
-        'vendor_name' => 'Tabler',
-        'package_name' => '@tabler/icons',
-        'source_directories' => [
-            'outline' => 'node_modules/@tabler/icons/icons/outline', 
-            // in case the icon has a prefix or suffix in the name such as 'icon-24' or '24-icon' to be able to determine the base name of the icon
-            /*
-            'outline' => [
-                'dir' => 'node_modules/vendor/icons/icons/outline',
-                'prefix' => null,
-                'suffix' => '-24',
-            ],
-            */
-            'solid' => 'node_modules/@tabler/icons/icons/filled',
-            /*'solid' => [ // in case there are different sizes of solid icons or they have a prefix or suffix in the name
-                '24' => ['dir' => 'node_modules/vendor/icons/icons/filled', 'prefix' => null, 'suffix' => '-24'],
-                '20' => ['dir' => 'node_modules/vendor/icons/icons/filled', 'prefix' => null, 'suffix' => '-20'],
-                '16' => ['dir' => 'node_modules/vendor/icons/icons/filled', 'prefix' => null, 'suffix' => '-16'],
-            ],*/
-        ],
-        'transform_svg_path' => function ($variant, $iconName, $svgPaths) {
-            if($variant == 'outline'){
-                // remove the first $svgPath from the array that has a d attribute of 'M0 0h24v24H0z'
-                $svgPaths = array_filter($svgPaths, function($svgPath) {
-                    return isset($svgPath['d']) && $svgPath['d'] !== 'M0 0h24v24H0z';
-                });
-            }
-            return $svgPaths;
-        },
-        'change_stroke_width' => function ($iconName, $defaultStrokeWidth, $svgPaths) {
-            // icons that have a circular shape should have a stroke width of 2 otherwise you may see a gap in the icon
-            // i.e. in case one of the paths has an a (arc) command, it's a circle
-            if(array_reduce($svgPaths, function($carry, $svgPath) {
-                return $carry || strpos($svgPath['d'], 'a') !== false;
-            }, false)){
-                return 2;
-            }
 
-            //if (in_array($iconName, ['grip-horizontal', 'grip-vertical'])) {
-            //    return 2;
-            //}
-            return $defaultStrokeWidth;
-        },
+     /**
+      * Default icons to be used in the project
+      * Listing icons here will make them auto buildable and updatable through flux-icons:build and flux-icons:update commands
+      * null or array ['vendor' => ['icon-name', ...] ] 
+      */  
+    'icons' => null,
+
+    /**
+     * Default stroke width for icons
+     * Heroicons and hence Flux uses by default a stroke width of 1.5 for icons
+     */
+    'default_stroke_wdith' => 1.5,
+
+    /**
+     * Default sizes for solid icons
+     * Heroicons and hence Flux uses by default 24, 20, and 16 sizes for solid icons
+     * Adjusting this is not yet properly implemented in this package
+     */
+    'solid_sizes' => [24, 20, 16],
+
+    /**
+     * Vendors configuration
+     */
+    'vendors' => [
+        'tabler' => [
+            'vendor_name' => 'Tabler',
+            'package_name' => '@tabler/icons',
+            'source_directories' => [
+                'outline' => 'node_modules/@tabler/icons/icons/outline', 
+                // in case the icon has a prefix or suffix in the name such as 'icon-24' or '24-icon' to be able to determine the base name of the icon
+                /*
+                'outline' => [
+                    'dir' => 'node_modules/vendor/icons/icons/outline',
+                    'prefix' => null,
+                    'suffix' => '-24',
+                ],
+                */
+                'solid' => 'node_modules/@tabler/icons/icons/filled',
+                /*'solid' => [ // in case there are different sizes of solid icons or they have a prefix or suffix in the name
+                    '24' => ['dir' => 'node_modules/vendor/icons/icons/filled', 'prefix' => null, 'suffix' => '-24'],
+                    '20' => ['dir' => 'node_modules/vendor/icons/icons/filled', 'prefix' => null, 'suffix' => '-20'],
+                    '16' => ['dir' => 'node_modules/vendor/icons/icons/filled', 'prefix' => null, 'suffix' => '-16'],
+                ],*/
+            ],
+            /**
+             * @param string $variant (solid, outline)
+             * @param string $iconName
+             * @param collection<SvgPath> $svgPaths
+             */
+            'transform_svg_path' => function ($variant, $iconName, $svgPaths) {
+                // remove the first $svgPath from the array that has a d attribute of 'M0 0h24v24H0z'
+                $svgPaths = $svgPaths->filter(function($svgPath){
+                    return $svgPath->getD() !== 'M0 0h24v24H0z';
+                });
+
+                return $svgPaths;
+            },
+
+             /**
+             * @param string $iconName
+             * @param float $defaultStrokeWidth
+             * @param collection<SvgPath> $svgPaths
+             */
+            'change_stroke_width' => function ($iconName, $defaultStrokeWidth, $svgPaths) {
+                // icons that have a small circular shape should have a stroke width of 2 otherwise you may see a gap in the icon when using 1.5
+                // ie icons such as dots, dots-vertical, grip-horizontal, grip-vertical, etc
+                return $svgPaths->filter(function($svgPath){
+                    return strpos($svgPath->getD(), 'a1 1 0 1 0') !== false;
+                })->count() > 0 ? 2 : $defaultStrokeWidth;
+            },
+        ],
+        // Add other vendors here...
+
     ],
-    // Add other vendors here...
 ];
