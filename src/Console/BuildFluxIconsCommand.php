ccf93f354482dedf21f5eaae6e912ae58275c94b
Maurits <maurits@ympact-technologies.nl>
2024-10-16 11:30:43 +0200

    fixing and improving artisan command

--- src/Console/BuildFluxIconsCommand.php
+++ src/Console/BuildFluxIconsCommand.php
@@ -4,34 +4,26 @@ namespace Ympact\FluxIcons\Console;
 
 use Ympact\FluxIcons\Services\IconBuilder;
 use Illuminate\Console\Command;
-use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Illuminate\Filesystem\Filesystem;
 
-class BuildFluxIconsCommand extends Command implements PromptsForMissingInput
+class BuildFluxIconsCommand extends Command
 {
     protected $signature = 'flux-icons:build
-                             {vendor? : The vendor icon package to use} 
-                             {--icons= : The icons to build (single or comma separated list)}
-                             {-a|--all : All icons from the vendor}
-                             {-m|--merge : Merge the icons from the --icons option with the default icons}
-                             {-v|--verbose : Show more details about the build process}';
+                            {vendor? : The vendor icon package to use} 
+                            {--I|icons= : The icons to build (single or comma separated list)}
+                            {--A|all : All icons from the vendor}
+                            {--M|merge : Merge the icons from the --icons option with the default icons}';
     protected $description = 'Build icons for Flux using a specific icon package';
-
-    /**
-     * Prompt for missing input arguments using the returned questions.
-     *
-     * @return array<string, string>
-     */
-    protected function promptForMissingArgumentsUsing(): array
-    {
-        return [
-            'vendor' => 'Which vendor icon package should be used?',
-        ];
-    }
 
     public function handle()
     {
         $vendor = $this->argument('vendor') ?? $this->ask('Which vendor icon package should be used (options: '.implode(', ',IconBuilder::getAvailableVendors()).')?');
+        
+        if (!config("flux-icons.vendors.$vendor")) {
+            $this->error("Vendor configuration for '$vendor' not found.");
+            return 1;
+        }
+
         $icons = $this->option('icons') ?? null;
         $all = $this->option('all');
         
@@ -40,7 +32,7 @@ class BuildFluxIconsCommand extends Command implements PromptsForMissingInput
             return 1;
         }
 
-        $defaultIcons = config("flux-icons.icons.{$vendor}", null);
+        $defaultIcons = config("flux-icons.icons", null);
 
         if($icons){
             $icons = explode(',', $icons);
@@ -54,17 +46,11 @@ class BuildFluxIconsCommand extends Command implements PromptsForMissingInput
             $icons = $defaultIcons;
         }
 
-        // if icons is still null , confirm that the user wants to build all icons
+        // if icons is still null, confirm that the user wants to build all icons
         if (!$icons && !$this->confirm("Are you sure you want to build all icons for vendor: $vendor?")) {
             $icons = $this->ask('Which icons should be built? (comma separated list)');
+            $icons = explode(',', $icons);
         }
-
-        if (!config("flux-icons.$vendor")) {
-            $this->error("Vendor configuration for '$vendor' not found.");
-            return 1;
-        }
-
-        // in case no icons are defined, use config("{$this->config}.icons")
 
         $files = app(Filesystem::class);
         $verbose = $this->option('verbose');
@@ -72,11 +58,11 @@ class BuildFluxIconsCommand extends Command implements PromptsForMissingInput
 
         $this->info("Installing package for vendor: $vendor");
         $iconBuilder->installPackage();
-
+        
         $this->info("Building icons for vendor: $vendor");
         $iconBuilder->buildIcons();
 
-        $this->info("Icons built successfully for vendor: $vendor");
+        $this->newLine()->info("Icons built successfully for vendor: $vendor");
         return 0;
     }
 }
