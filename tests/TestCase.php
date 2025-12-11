5a62226a3671f40b44e21dd8790a255cef53bd45
Maurits <maurits@ympact-technologies.nl>
2025-12-11 14:08:27 +0100

    tests

---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+abstract class TestCase extends OrchestraTestCase
+{   
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Ensure base_path() resolves to the package root during tests
+        $this->app->instance('path.base', dirname(__DIR__));
+    }
+}
